24f69ba49ee3a5950464f99217fa2942fc9e2df3
Kory Nunn <knunn187@gmail.com>
2024-10-12 12:45:50 +1000

    Create README.sh

---
+++ README.sh
@@ -0,0 +1,28 @@
+# To replace Clutch pressure sensor 1 (K1) for error code P1735 
+
+1. remove underbody plastic
+1. remove Lower charge pipe
+1. remove Air intake scoop (two screws)
+1. remove Airbox lower half (two screws)
+1. remove Airbox filter/cover/screws
+1. remove mech cover bracket nuts for a few things
+1. mech cover screws (double ended nut in the middle things)
+1. mech electrical plug
+1. mech plug retainer clip
+1. upper charge pipe
+1. Remove cover
+1. Remove 9 bolts
+1. remove mech from trans
+1. remove 2 bolts from motherboard
+1. remote 2 bolts from pressure clip
+1. Remove almost all the screws from the back thingo
+1. Bend the back thingo just enough to get the pins out
+1. Pivot the pins bit back and lift the sensor side off
+1. unclip the metal sensor retainer clip
+1. remove rubber plug on the other side
+1. cut wires
+1. pop out old sensor
+1. pop in new sensor
+1. bend wires to touch pins
+1. solder new sensor to wires
+1. reverse from "remove rubber plug on the other side"
